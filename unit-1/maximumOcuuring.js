785e9064b49d6570c83055d52747b6c9407fa3b2
ap-lodhi <anilpatel.bcs@gmail.com>
2022-11-21 15:05:10 +0530

    first q

---
+++ unit-1/maximumOcuuring.js
@@ -0,0 +1,35 @@
+// Sample Input 1 
+
+// 5
+// 0 2 0 6 9
+// Sample Output 1
+
+// 0
+
+
+function maximumOccuringElement(A,N){
+   
+    var m  =new Map();
+for(var i=0; i<N; i++){
+  if(m.has(A[i])){
+    let current = m.get(A[i]);
+    m.set(A[i],current+1);
+  }
+  else{
+    m.set(A[i],1)
+  }
+}
+var max = 0; 
+var res ;
+for(var i=0; i<N; i++){
+    if(m.get(A[i] )> max){
+        max = m.get(A[i])
+        res = A[i]
+    }
+}
+console.log(res)
+}
+
+var A = [0, 2, 0, 6, 9]
+var N = 5
+var ans  =maximumOccuringElement(A,N)
