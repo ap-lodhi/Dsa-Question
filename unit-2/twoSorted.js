2dc0f4998fc8c79f593a8abd59a99d52fd8bc969
ap-lodhi <anilpatel.bcs@gmail.com>
2022-11-22 21:59:45 +0530

    6th question unit-2

---
+++ unit-2/twoSorted.js
@@ -0,0 +1,78 @@
+// Two Sorted Arrays Ended
+// Description
+
+// You are given two arraysAandB, each of size N.
+// The first array A, is sorted in ascending order, while the second array is sorted in descending order.
+// You have to find the number of elements common in both the arrays.
+// Try to solve this without using any extra space.
+
+
+// Input
+// The first line of the input containsT, the number of test cases.
+
+// The first line of each test case, contains N, the number of elements in the arrayAandB
+
+// The next line of the test case, containsNspace separated integers, denoting the number of elements of the arrayA
+
+// The next line containsNspace separated integers, denoting elements of the arrayB.
+
+// Constraints
+
+// 1 <=T<= 10
+
+// 1 <=N<= 1000000
+
+// 1 <=A[i]<= 1000
+
+
+// Output
+// For each test case, print the number of elements common in both the arrays, on a new line.
+
+
+// Sample Input 1 
+
+// 1
+// 6
+// 1 2 2 3 4 5
+// 4 4 3 2 1 1
+// Sample Output 1
+
+// 4
+// Hint
+
+// In the sample test case, the number of elements common in both the arrays are {1,2,3,4}. Therefore, the required output is 4.
+
+function runProgram(input) {
+    input = input.split("\n");
+     var test = +input[0];
+     var line = 1;
+     for(var l=0; l<test; l++){
+     var n  = +input[line];
+      line++;
+      var arr1 = input[line].trim().split(" ").map(Number);
+      line++;
+       var arr2 = input[line].trim().split(" ").map(Number);
+      line++;
+      var count =0
+      var i =0;
+      var j=n-1; 
+      while(i <n && j>=0){
+          if(arr1[i] == arr2[j]){
+              count++
+              i++;
+              j--;
+          }
+          else if(arr1[i] >arr2[j]){
+              j--
+          }
+          else{
+              i++
+          }
+          
+        
+      }
+      console.log(count)
+   
+   }
+   
+   }
