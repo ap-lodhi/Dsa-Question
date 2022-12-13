388eec15f4b767786f1c98b42f2ede5b3c374b1e
ap-lodhi <anilpatel.bcs@gmail.com>
2022-12-13 18:03:01 +0530

    path Module

---
+++ node/pathMod/path.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+console.log(path.dirname('D:/allDsaQuestion/node/pathMod/path.js'))
+console.log(path.extname('D:/allDsaQuestion/node/pathMod/path.js'))
+console.log(path.basename('D:/allDsaQuestion/node/pathMod/path.js'))
+
+
+
+
+console.log(path.parse('D:/allDsaQuestion/node/pathMod/path.js'))
