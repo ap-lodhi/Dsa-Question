3117bc5c38f6d6f09ec1d34499df0adcf2b82ff7
ap-lodhi <anilpatel.bcs@gmail.com>
2023-01-03 15:17:27 +0530

    get req

--- express/index.js
+++ express/index.js
@@ -1,5 +1,7 @@
-const { application } = require('express')
-const express  =require('express')
+const { Console } = require('console');
+const { json } = require('express');
+const express  =require('express');
+const fs = require('fs')
 
 const app = express()
 app.use(express.json())
@@ -10,6 +12,16 @@ app.use(express.json())
 app.get("/",(req,res)=>{
     res.send("hello ")
 })
+
+app.get("/allData",(req,res)=>{
+    const  data = fs.readFileSync("./db.json","utf-8");
+    const parse = JSON.parse(data)
+   console.log(parse)
+    res.send(parse)
+})
+
+
+app.post()
 
 
 app.listen(3500,()=>{
