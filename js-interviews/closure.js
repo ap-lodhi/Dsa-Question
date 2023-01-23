7abc6cf7b7e55c28b98c92a1df026109f30fc195
ap-lodhi <anilpatel.bcs@gmail.com>
2023-01-23 16:08:41 +0530

    js closure

--- js-interviews/closure.js
+++ js-interviews/closure.js
@@ -6,17 +6,47 @@
 // function along with its lexical scope bundeld togethe.
 
 
-/*/**A closure is the  combination of  function bundled tigether with refrence  to its surrounding stte 
+/*/**A closure is the  combination of  function bundled together with refrence  to its surrounding stte 
 (lexical envoirment )in other words a closure gives you acceses to encounter function scope from an
 inner function  */
 
 
-function outer(){
-    var a = 10;
-    return function inner(b){
-        return a+b;
-    }
-}
-var ans  = outer()
+// function outer(){
+//     var a = 10;
+//     return function inner(b){
+//         return a+b;
+//     }
+// }
+// var ans  = outer()
 
-console.log(ans(12))
+// console.log(ans(12))
+
+// closure 
+// currying
+
+
+// function outer(a){
+   
+//     return function inner(b){
+//         return a+b;
+//     }
+// }
+// console.log(outer(5)(20))
+
+
+
+
+function calculator(intiValue =0){
+    var value = intiValue;
+
+    function add(val){
+        value =value +val
+    }
+    return {add}
+}
+var calc = calculator()
+console.log(calc.add(10))
+
+// memoization 
+// debouncing 
+// throtling 
