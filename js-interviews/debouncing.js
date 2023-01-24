f130434836214dabafda6b486125db93e262aec2
ap-lodhi <anilpatel.bcs@gmail.com>
2023-01-24 17:30:54 +0530

    throtling example

--- js-interviews/debouncing.js
+++ js-interviews/debouncing.js
@@ -4,6 +4,16 @@
  * by delaying the execution of the function until a specified time to avoid unnecessary CPU cycles, 
  * and API calls and improve performance. */
 
+// A Debounce function is a higher-order function that returns another function, to create closure around the function parameters (func, timeout) and the timer variable.
+
+// func: is a function that you want to execute after the debounce time
+
+// timeout: The amount of time you want the debounce function to wait after the last received action before executing func.
+
+// timer: The value used to indicate a running debounce.
+
+// The common use cases are Search box suggestions, text-field auto-saves, and eliminating double-button clicks.
+
     const makeApicall=()=>{
         console.log("api call ")
     }
@@ -16,7 +26,11 @@
             debouncing && clearTimeout(debouncing)
           debouncing =  setTimeout(()=>fn.apply(this,arguments),delay)
         }
+  
+  
     }
+
+    
 window.addEventListener("load",function(){
     var btn = document.getElementById('click');
     btn.addEventListener('click',debouncer(makeApicall,1000))
---
+++ js-interviews/throtling.js
@@ -0,0 +1,21 @@
+const makeApicall=()=>{
+    console.log("api call ")
+}
+let c =0;
+const throttle =(fn,wait)=>{
+    //.. present time ,last  call time  was  made  
+    let lastCall = 0
+    return function(){
+
+        if(Date.now()- lastCall >wait) {
+            lastCall = Date.now()
+            fn.apply(this,arguments)
+        }
+        
+    }
+}
+
+window.addEventListener("load",function(){
+    var btn = document.getElementById('click');
+    btn.addEventListener('click',throttle(makeApicall,1000))
+})
