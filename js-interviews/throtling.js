a38b6fee98bcd6893a19ed371198bbcd7d23c0c3
ap-lodhi <anilpatel.bcs@gmail.com>
2023-01-25 20:49:53 +0530

    throtling example

--- js-interviews/throtling.js
+++ js-interviews/throtling.js
@@ -1,4 +1,5 @@
 // throtlling 
+/** Throttling is used to call a function after every millisecond or a particular interval of time only the first click is executed immediately */
 const makeApicall=()=>{
     console.log("api call ")
 }
