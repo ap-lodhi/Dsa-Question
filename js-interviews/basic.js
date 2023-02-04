591dd2a612bb308e03bcfac17a667ba505e64c78
ap-lodhi <anilpatel.bcs@gmail.com>
2023-02-04 16:44:54 +0530

    basic

---
+++ js-interviews/basic.js
@@ -0,0 +1,29 @@
+
+
+// undefined 
+
+console.log(undefined==null)  // true
+console.log(undefined===null)  // false
+
+
+
+//NAN
+
+
+// NaN (Not a Number) is a numeric data type that means an undefined value or value that cannot be represented
+//  especially results of floating-point calculations.
+console.log(NaN ==NaN)  // false
+console.log(NaN ===NaN) // false
+
+
+
+// Array
+
+// Object has  diffrent  address as such si it will not  be  equal 
+
+
+console.log(  "arayy ",[]==[])  // false 
+console.log(  "arayy ",[]==[])  // false 
+console.log("obj",{}=={}) //false 
+console.log("obj",{}==={})//false  it  may be contain same  data  point  
+ 
