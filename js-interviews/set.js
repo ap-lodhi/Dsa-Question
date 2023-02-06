f8e873c1d698ec0d7d32efc93b4cd8dcb9240f89
ap-lodhi <anilpatel.bcs@gmail.com>
2023-02-06 15:30:57 +0530

    event concept

--- js-interviews/set.js
+++ js-interviews/set.js
@@ -6,5 +6,17 @@ for(var i=0; i<arr.length; i++){
     },i*1000)
 }
 
+const fun2  =()=>{
+    setTimeout(()=>{
+        console.log("fun2 is stating")//3
+    },3000)
+}
 
-
+const fun1  =()=>{
+  
+        console.log("fun1 is stating") //1
+        fun2();
+        console.log('fun1 is ending')//2
+   
+}
+fun1()
