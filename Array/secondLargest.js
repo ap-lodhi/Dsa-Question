7e2ce72a75a2eb0fb6895869b441416ee1930425
ap-lodhi <anilpatel.bcs@gmail.com>
2023-02-07 15:49:53 +0530

    second  largest

---
+++ Array/secondLargest.js
@@ -0,0 +1,14 @@
+var arr =[1,2,4,2,6,5]
+var first  =-1; 
+var second = -1; 
+for(var  i=0; i<arr.length; i++){
+	if(arr[i]> first){
+		second =first;
+		first = arr[i]
+	}
+	else if (arr[i] > second && arr[i] != first) {
+		second =arr[i]
+	}
+}
+
+console.log(second)
