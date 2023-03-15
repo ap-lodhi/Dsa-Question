c9694bdf895ac7c9160c869bac5fbaced5ece12f
ap-lodhi <anilpatel.bcs@gmail.com>
2023-03-15 20:59:16 +0530

    questions

---
+++ backend/index.js
@@ -0,0 +1,60 @@
+/**# HCJ
+
+## Instructions
+
+- Read all questions carefully.
+- Prepare notes for all questions.
+
+## Questions:
+
+1. **What is the difference between a block element and an inline element? Give few examples of block and inline tags which we have in HTML.**
+2. **What are pseudo-elements and pseudo-classes in CSS? Give some examples.**
+3. **What are CSS positions? What are the position types in CSS? What is the default position property in CSS?**
+4. **What is difference between Local storage, session storage and cookies?**
+5. **What is Hoisting in Javascript?**
+6. **What are different higher order functions in JS? What is the difference between .map() and .forEach() ?**
+7. **Difference between spread and rest operator with examples**
+8. **Explain execution context diagram of following code snippets, use white board to draw execution context diagram**
+
+      Code Snippet-1
+
+```jsx
+console.log('First');
+setTimeout(() => console.log('Second'), 0);
+console.log('Third');
+
+```
+
+       Code Snippet-2
+
+```jsx
+console.log('First');
+function secondCall() {
+  console.log('Second');
+}
+setTimeout(secondCall, 2000);
+setTimeout(() => console.log('Third'), 0);
+console.log('Third');
+
+```
+
+1. **What are promises? What are the different states of a promise? Support your answer with an example where you need to create your own promise.**
+2. ****What is ‘this’ keyword in JavaScript? explain with an example****
+3. ****What is an Immediately Invoked Function in JavaScript? explain with an example****
+4. **Explain OOPs concept and explain the Four Principles of OOP.** */
+
+/**# NEM
+
+## Questions:
+
+1. W**hat is the difference between readFile and readFileSync.**
+2. **How to create a web server without express? Write some basic Routes.**
+3. **What is he difference between common JS and EJS module?**
+4. **What is JWT and what we can achieve with that?**
+5. **What should we do with the password of a user before storing it into DB?**
+6. **What is File stream?**
+7. **What are middlewares, and how they can help us?**
+8. **How can we take inputs from terminal?**
+9. **How many types of modules are there in node?**
+10. **What is the difference between authentication and authorization?** */
+
