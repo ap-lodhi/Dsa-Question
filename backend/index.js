c2c198d0596dff314bc1c3632f4bfb27eeee1560
ap-lodhi <anilpatel.bcs@gmail.com>
2023-03-16 17:57:49 +0530

    fd

--- backend/index.js
+++ backend/index.js
@@ -58,3 +58,29 @@ console.log('Third');
 9. **How many types of modules are there in node?**
 10. **What is the difference between authentication and authorization?** */
 
+// 1. ****What is the difference between .call() .apply() and .bind()? explain with an example****
+// 2. **Explain Event bubbling and Event Capturing in JavaScript with suitable examples**
+// 3. **What is function currying with example?**
+// 4. **What is the difference between real DOM and virtual DOM?**
+// 5. **Why did you choose React over other Libraries and Frameworks?**
+// 6. **What is prop drilling and explain it with an example (parent to child)**
+// 7. **What is the difference between function and class components?**
+// 8. **Explain the lifecycle method of React Components.**
+// 9. **Explain the Flow of Redux.**
+// 10. **What are hooks? Explain a few hooks you have used.**
+// 11. **Explain useMemo, useRef, useEffect, and useCallback hooks in react.**
+// 12. **Make a small application using react where on a button click it should count from 0 to 10 and on clicking it again it should count from 10 to 0.**
+
+
+
+
+// 1. W**hat is the difference between readFile and readFileSync.**
+// 2. **How to create a web server without express? Write some basic Routes.**
+// 3. **What is he difference between common JS and EJS module?**
+// 4. **What is JWT and what we can achieve with that?**
+// 5. **What should we do with the password of a user before storing it into DB?**
+// 6. **What is File stream?**
+// 7. **What are middlewares, and how they can help us?**
+// 8. **How can we take inputs from terminal?**
+// 9. **How many types of modules are there in node?**
+// 10. **What is the difference between authentication and authorization?**
