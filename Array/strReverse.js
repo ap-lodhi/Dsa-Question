8e3c2c146c9737167c68b222ab0a9633f52f6a02
ap-lodhi <anilpatel.bcs@gmail.com>
2023-05-14 14:22:34 +0530

    two pointer

---
+++ Array/strReverse.js
@@ -0,0 +1,21 @@
+// To reverse a string in the same array without using an extra array, you can use a two-pointer approach.
+
+// Here is an example code snippet in JavaScript:
+
+console.log("first")
+function reverse(str){
+    var left = 0;
+    var right = str.length-1;
+    while(left<right){
+        var temp = str[left]
+        str[left] = str[right]
+        str[right] = temp
+        left++
+        right--
+    }
+    return str
+}
+
+var  arr = ["h", "e", "l", "l", "o"];
+
+console.log(reverse(arr))
