64834bf8fc7014fe65941fb5bad104a91b6fa976
ap-lodhi <anilpatel.bcs@gmail.com>
2023-05-14 15:09:44 +0530

    ts

---
+++ typeScript/src/enum.ts
@@ -0,0 +1,26 @@
+enum Color {
+  Red,
+  Green,
+  Blue,
+}
+let c: Color = Color.Green;
+
+// By default, enums begin numbering their members starting at 0.
+// You can change this by manually setting the value of one of its members.
+// For example, we can start the previous example at 1 instead of 0:
+
+enum Color {
+  Red = 1,
+  Green,
+  Blue,
+}
+let c: Color = Color.Green;
+
+// Or, even manually set all the values in the enum:
+
+enum Color {
+  Red = 1,
+  Green = 2,
+  Blue = 4,
+}
+let c: Color = Color.Green;
