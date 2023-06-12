87b2c9964ed6490882369c2f831834feb1f4f5e8
ap-lodhi <anilpatel.bcs@gmail.com>
2023-06-12 17:57:16 +0530

    basic

--- js-interviews/promises.js
+++ js-interviews/promises.js
@@ -5,3 +5,31 @@
 const set = new Set ([1,2,3,41,2,3,4])
 
 console.log(set)  // will remove duplicate  
+
+
+// const data = ['anil','paji', 'neta']
+// const [y]= data
+// console.log(y) // Anil
+// const [ ,y]= data
+// console.log(y) // paji
+
+
+
+
+
+// const data = {name:"anil", age:29, skill:"mern"}
+// const {name} = data
+// console.log(name) // to get property without using  . dot operator we have to this  
+
+
+
+// const res = false || {} || null
+// if({}){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+
+// console.log(res)
+
+
