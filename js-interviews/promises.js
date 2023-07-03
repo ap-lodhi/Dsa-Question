4deea2a778408dd56e4dc073bd7bf1870a86417b
ap-lodhi <anilpatel.bcs@gmail.com>
2023-07-03 10:31:36 +0530

    up

--- js-interviews/promises.js
+++ js-interviews/promises.js
@@ -2,39 +2,46 @@
 // console.log(data)  // 20 
 
 
-const set = new Set ([1,2,3,41,2,3,4])
+// const set = new Set ([1,2,3,41,2,3,4])
 
-console.log(set)  // will remove duplicate  
+// console.log(set)(
+  // will remove duplicate
 
+  // const data = ['anil','paji', 'neta']
+  // const [y]= data
+  // console.log(y) // Anil
+  // const [ ,y]= data
+  // console.log(y) // paji
 
-// const data = ['anil','paji', 'neta']
-// const [y]= data
-// console.log(y) // Anil
-// const [ ,y]= data
-// console.log(y) // paji
+  // const data = {name:"anil", age:29, skill:"mern"}
+  // const {name} = data
+  // console.log(name) // to get property without using  . dot operator we have to this
 
+  // const res = false || {} || null
+  // if({}){
+  //     console.log("yes")
+  // }else{
+  //     console.log("no")
+  // }
 
+  // console.log(res)
 
+  // console.log(Promise.resolve(4))
+  // console.warn(Promise.resolve(4)) // promise fullfilled
 
-
-// const data = {name:"anil", age:29, skill:"mern"}
-// const {name} = data
-// console.log(name) // to get property without using  . dot operator we have to this  
-
-
-
-// const res = false || {} || null
-// if({}){
-//     console.log("yes")
-// }else{
-//     console.log("no")
-// }
-
-// console.log(res)
-
-
-// console.log(Promise.resolve(4))
-// console.warn(Promise.resolve(4)) // promise fullfilled 
+  // console.log(`${(x=>X)('i love')} to programing`)//
 
 
-console.log(`${(x=>X)('i love')} to programing`)
+//   (function () {
+//     var a = (b = 3);
+//   })();
+//   console.log(typeof a);
+
+  var bar = 1,
+    fun = {};
+  fun: {
+    bar: 2;
+    x: ++bar;
+  }
+  fun.x + fun.bar + bar;
+  
