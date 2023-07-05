d8807b220d2a1752d9bc74e4af2db939cd197a61
ap-lodhi <anilpatel.bcs@gmail.com>
2023-07-05 13:22:08 +0530

    update

---
+++ js-interviews/foo.js
@@ -0,0 +1,28 @@
+foo()
+function foo(){
+    console.log('heloo')
+}
+
+
+
+const arr=[1,3,4,2,]
+arr[0]=8
+console.log(arr)
+
+
+console.log(name)
+name ='masai'
+var  name
+
+
+console.log(name)
+// console.log(anil)
+
+var  arr1 =[1,2,3,4,5]
+arr1.map(a=>a*5)
+console.log(arr1)  
+for(var i=0; i<5; i++){
+    setTimeout(()=>{
+console.log(i)
+    },0)
+}
